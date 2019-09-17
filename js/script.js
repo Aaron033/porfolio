0f388e0bc2c333b46db9b1e170ccb74d82f81716
Aaron Botello <aaron.botello.h@gmail.com>
2019-09-17 13:46:32 -0500

    ul height deleted

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous">
+// src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous">
 
-console.log("hello")
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+
+//         if($(window).scrollTop()> 400){
+//             $('nav').addClass('red'); 
+
+//         }else{
+//             $('nav').removeClass('red');
+//         }
+//     })
+// })
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
