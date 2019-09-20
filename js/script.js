a4e25eec4bafdd1c06afd799c8de8c074004c0ce
Aaron Botello <aaron.botello.h@gmail.com>
2019-09-20 17:59:37 -0500

    nav added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 
-(function() {
-    let burger = document.querySelector('.burger'); 
-    let nav = document.querySelector('#' +burger.dataset.target); 
+// (function() {
+//     let burger = document.querySelector('.burger'); 
+//     let nav = document.querySelector('#' +burger.dataset.target); 
   
-    burger.addEventListener('click', function(){
-      burger.classList.toggle('is-active'); 
-      nav.classList.toggle('is-active');
-    });
-  })();
+//     burger.addEventListener('click', function(){
+//       burger.classList.toggle('is-active'); 
+//       nav.classList.toggle('is-active');
+//     });
+//   })();
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
