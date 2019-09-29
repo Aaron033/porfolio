4575e6f395a40cc21ef1accf624dbe69dec752f1
Aaron Botello <aaron.botello.h@gmail.com>
2019-09-28 21:39:27 -0500

    js code switch '

--- js/music.js
+++ js/music.js
@@ -1,36 +1,25 @@
-window.onscroll = function(){
-    scrollFunction()
- }
- function scrollFunction(){
-    var sn = document.getElementById("sticky_nav");
-       if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
-          sn.style.top = "0"
-       }
-       else{
-          sn.style.top = "-45px"
-       }
- }
+document.addEventListener('DOMContentLoaded', () => {
+
+   // Get all "navbar-burger" elements
+   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
  
- function openList(x){
-    var cL = document.getElementsByClassName("content_list");
-    var i;
-       for(i = 0; i < cL.length; i++){
-          var OcL = cL[i];
-             if(cL[i] != document.getElementById("thisList_" + x)){
-                OcL.classList.remove("show_list")
-             }
-       }
-    document.getElementById("thisList_" + x).classList.toggle("show_list")
- }
+   // Check if there are any navbar burgers
+   if ($navbarBurgers.length > 0) {
  
- function openSearch(){
-    document.getElementById("thisSearch").classList.toggle("show_search");
-    document.getElementById("btnSearch").getElementsByTagName("i")[0].classList.toggle("hidden");
-    document.getElementById("btnSearch").getElementsByTagName("i")[1].classList.toggle("visible")
- }
+     // Add a click event on each of them
+     $navbarBurgers.forEach( el => {
+       el.addEventListener('click', () => {
  
- function openMenu(){
-    document.getElementById("thisMenu").classList.toggle("show_menu");
-    document.getElementById("btnMenu").getElementsByTagName("i")[0].classList.toggle("hidden");
-    document.getElementById("btnMenu").getElementsByTagName("i")[1].classList.toggle("visible")
- }
+         // Get the target from the "data-target" attribute
+         const target = el.dataset.target;
+         const $target = document.getElementById(target);
+ 
+         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+         el.classList.toggle('is-active');
+         $target.classList.toggle('is-active');
+ 
+       });
+     });
+   }
+ 
+ });
