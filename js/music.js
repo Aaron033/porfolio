377192ede46bec65be5d7c2be1cc29c1854dc2e2
Aaron Botello <aaron.botello.h@gmail.com>
2019-10-06 20:33:35 -0500

    carousel not working

--- js/music.js
+++ js/music.js
@@ -41,3 +41,12 @@ document.addEventListener('DOMContentLoaded', () => {
    }
    x[slideIndex-1].style.display = "block";  
  }
+
+ // Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel', options);
+
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+}
